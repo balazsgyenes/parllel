9625a4609e2c1bf10bfa4c55238aab6872edfe0f
Balazs Gyenes <balazs.gyenes@kit.edu>
2021-05-24 20:55:18 +0200

    Add test for SharedMemoryArray.
    Verify functionality with spawn and fork start methods.
    Add repr method to Array.

--- parllel/arrays/array.py
+++ parllel/arrays/array.py
@@ -20,7 +20,7 @@ class Array:
         self._dtype = dtype
 
     def initialize(self) -> None:
-        # initialize buffer in either local or shared memory
+        # initialize numpy array
         self._array = np.zeros(shape=self._shape, dtype=self._dtype)
         
     def __getitem__(self, location: Indices) -> NDArray:
@@ -34,3 +34,11 @@ class Array:
             return self._array
         else:
             return self._array.astype(dtype, copy=False)
+
+    def __repr__(self) -> str:
+        if hasattr(self, "_array"):
+            return repr(self._array)
+        else:
+            return (f"Uninitialized {type(self).__name__} object: "
+                    f"shape={self._shape}, dtype={np.dtype(self._dtype).name}."
+            )
--- parllel/arrays/tests/shared_mem_tests.py
+++ parllel/arrays/tests/shared_mem_tests.py
@@ -3,6 +3,7 @@ import ctypes
 
 import numpy as np
 
+from parllel.arrays.sharedmemory import SharedMemoryArray
 
 shape = (5,5)
 dtype = np.int32
@@ -11,24 +12,24 @@ size = int(np.prod(shape))
 nbytes = size * np.dtype(dtype).itemsize
 
 def f(obj):
-    # buffer = pipe.recv()
-    buffer = obj
-    array = np.frombuffer(buffer, dtype=dtype, count=size).reshape(shape)
-    array.fill(5)
+    # buffer = obj
+    # array = np.frombuffer(buffer, dtype=dtype, count=size).reshape(shape)
+    array = obj
+    array[:, :] = 5
 
 if __name__ == "__main__":
     mp.set_start_method("spawn")
 
-    raw_array = mp.RawArray(ctypes.c_char, nbytes)
-    array = np.frombuffer(raw_array, dtype=dtype, count=size).reshape(shape)
-    # array = np.array(raw_array, dtype, copy=False)
+    # raw_array = mp.RawArray(ctypes.c_char, nbytes)
+    # array = np.frombuffer(raw_array, dtype=dtype, count=size).reshape(shape)
+    array = SharedMemoryArray(shape=shape, dtype=dtype)
+    print(array)
+    array.initialize()
     print(array)
 
-    # parent_pipe, child_pipe = mp.Pipe()
 
-    p = mp.Process(target=f, args=(raw_array,))
+    p = mp.Process(target=f, args=(array,))
     p.start()
-    # parent_pipe.send(raw_array)
     p.join()
 
     print(array)
