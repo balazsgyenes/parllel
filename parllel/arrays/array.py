4aef0a8613ee038ea99d7633097d5c3839cc152a
Paul Maria Scheikl <paul.scheikl@kit.edu>
2021-05-25 18:38:22 +0200

    Added tests and documentation for Array.

--- parllel/arrays/array.py
+++ parllel/arrays/array.py
@@ -10,8 +10,20 @@ Indices = Union[Index, Tuple[Index, ...]]
 
 class Array:
     """Abstracts memory management for large arrays.
+
+    Arrays must be initialized before using them.
+
+    Example:
+        >>> array = Array(shape=(4, 4, 4), dtype=np.float32)
+        >>> array.initialize()
+        >>> array[:, slice(1, 3), 2] = 5.
+
+    TODO:
+        - enforce dtype to be an actual np.dtype
     """
-    def __init__(self,
+
+    def __init__(
+        self,
         shape: Tuple[int, ...],
         dtype: np.dtype,
     ) -> None:
@@ -22,14 +34,14 @@ class Array:
     def initialize(self) -> None:
         # initialize numpy array
         self._array = np.zeros(shape=self._shape, dtype=self._dtype)
-        
+
     def __getitem__(self, location: Indices) -> NDArray:
         return self._array[location]
-    
+
     def __setitem__(self, location: Indices, value: Any) -> None:
         self._array[location] = value
 
-    def __array__(self, dtype = None) -> NDArray:
+    def __array__(self, dtype=None) -> NDArray:
         if dtype is None:
             return self._array
         else:
@@ -39,6 +51,4 @@ class Array:
         if hasattr(self, "_array"):
             return repr(self._array)
         else:
-            return (f"Uninitialized {type(self).__name__} object: "
-                    f"shape={self._shape}, dtype={np.dtype(self._dtype).name}."
-            )
+            return f"Uninitialized {type(self).__name__} object: " f"shape={self._shape}, dtype={np.dtype(self._dtype).name}."
---
+++ test/array_test/array_test.py
@@ -0,0 +1,72 @@
+import pytest
+import numpy as np
+from parllel.arrays.array import Array
+
+
+class TestArray:
+    def test_no_args(self):
+        with pytest.raises(TypeError):
+            _ = Array()
+
+    def test_wrong_dtype(self):
+        with pytest.raises(AssertionError):
+            shape = (4, 4, 4)
+            _ = Array(shape=shape, dtype=list)
+
+    def test_initialize_first(self):
+        with pytest.raises(AttributeError):
+            shape = (4, 4, 4)
+            array = Array(shape=shape, dtype=np.float32)
+            array[1, 2, 3] = 5.0
+
+    def test_initialize(self):
+        shape = (4, 4, 4)
+        array = Array(shape=shape, dtype=np.float32)
+        array.initialize()
+        assert np.all(array == np.zeros(shape))
+
+    def test_setitem(self):
+        shape = (4, 4, 4)
+        array = Array(shape=shape, dtype=np.float32)
+        array.initialize()
+
+        array[:] = 5.0
+
+        same_values = np.ones(shape, dtype=np.float32)
+        same_values[:] = 5.0
+
+        assert np.all(array == same_values)
+
+    def test_getitem(self):
+        shape = (4, 4, 4)
+        array = Array(shape=shape, dtype=np.float32)
+        array.initialize()
+
+        array[1, 2, 3] = 5.0
+
+        assert array[1, 2, 3] == 5.0
+
+    def test_setitem_slice(self):
+        shape = (4, 4, 4)
+        array = Array(shape=shape, dtype=np.float32)
+        array.initialize()
+
+        array[:, slice(1, 3), 3] = 5.0
+
+        same_values = np.zeros(shape, dtype=np.float32)
+        same_values[:, slice(1, 3), 3] = 5.0
+
+        assert np.all(array == same_values)
+
+    def test_getitem_slice(self):
+        shape = (4, 4, 4)
+        array = Array(shape=shape, dtype=np.float32)
+        array.initialize()
+
+        assert array[:, slice(1, 3), 3].shape == (4, 2)
+
+    def test_dtype(self):
+        array = Array(shape=(4, 4, 4), dtype=np.float32)
+        array.initialize()
+        array[0, 0, 0] = 5
+        assert type(array[0, 0, 0]) == np.float32
