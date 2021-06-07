56e677d26379f7e0e462549510f2ed8fe7899de9
Balazs Gyenes <balazs.gyenes@kit.edu>
2021-06-07 11:03:15 +0200

    Update TorchAgent interface.

--- parllel/torch/agents/agent.py
+++ parllel/torch/agents/agent.py
@@ -1,20 +1,13 @@
-from dataclasses import dataclass
 from typing import Any, Iterable, Union
-from nptyping import NDArray
 
 import torch
 
-
+from parllel.buffers import Buffer
+from parllel.handlers import Agent, AgentStep
 from parllel.torch.distributions.distribution import Distribution
 
 
-@dataclass(frozen=True)
-class AgentStep:
-    action: Any
-    agent_info: Any
-
-
-class Agent:
+class TorchAgent(Agent):
     """The agent manages as a model and a sampling state for each environment
     instance. Outputs from the model are converted into actions during
     sampling, usually with the help of a distribution.
@@ -45,11 +38,11 @@ class Agent:
             self._rnn_states[:, env_index] = 0
 
     @torch.no_grad()
-    def step(self, observation: NDArray, prev_action: NDArray, prev_reward: NDArray, env_ids: Union[int, slice]) -> AgentStep:
+    def step(self, observation: Buffer, prev_action: Buffer, prev_reward: Buffer, env_ids: Union[int, slice]) -> AgentStep:
         """Returns selected actions for environment instances in sampler."""
         raise NotImplementedError
 
-    def evaluate(self, observation: NDArray, prev_action: NDArray, prev_reward: NDArray, prev_rnn_state: torch.Tensor = None) -> Any:
+    def evaluate(self, observation: Buffer, prev_action: Buffer, prev_reward: Buffer, prev_rnn_state: Buffer = None) -> Any:
         """Returns values from model forward pass on training data (i.e. used
         in algorithm)."""
         raise NotImplementedError
@@ -62,17 +55,17 @@ class Agent:
         """Returns model parameters for saving."""
         raise NotImplementedError
 
-    def train_mode(self, steps_elapsed: int) -> None:
+    def train_mode(self, elapsed_steps: int) -> None:
         """Go into training mode (e.g. see PyTorch's ``Module.train()``)."""
         self.model.train()
         self._mode = "train"
 
-    def sample_mode(self, steps_elapsed: int) -> None:
+    def sample_mode(self, elapsed_steps: int) -> None:
         """Go into sampling mode."""
         self.model.eval()
         self._mode = "sample"
 
-    def eval_mode(self, steps_elapsed: int) -> None:
+    def eval_mode(self, elapsed_steps: int) -> None:
         """Go into evaluation mode.  Example use could be to adjust epsilon-greedy."""
         self.model.eval()
         self._mode = "eval"
