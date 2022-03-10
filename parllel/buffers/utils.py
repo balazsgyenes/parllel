e3723719a5b8e7c031bc5fe09b395e1729483fcb
Balazs Gyenes <balazs.gyenes@kit.edu>
2022-03-10 15:08:23 +0100

    Type hints for buffer utils.

--- parllel/buffers/utils.py
+++ parllel/buffers/utils.py
@@ -1,6 +1,7 @@
 from typing import Dict, Tuple
 
 import numpy as np
+from nptyping import NDArray
 
 from parllel.buffers import Buffer
 from parllel.arrays import Array
@@ -47,7 +48,7 @@ def buffer_func(func, buffer, *args, **kwargs):
     return func(buffer, *args, **kwargs)
 
 
-def buffer_from_example(example, leading_dims: Tuple[int, ...], ArrayClass: Array, **kwargs) -> Buffer:
+def buffer_from_example(example: Buffer[NDArray], leading_dims: Tuple[int, ...], ArrayClass: Array, **kwargs) -> Buffer:
     if example is None:
         return None
     if isinstance(example, (NamedArrayTuple, NamedTuple)):
@@ -61,7 +62,7 @@ def buffer_from_example(example, leading_dims: Tuple[int, ...], ArrayClass: Arra
         return ArrayClass(shape=shape, dtype=dtype, **kwargs)
 
 
-def buffer_from_dict_example(example, leading_dims: Tuple[int, ...], ArrayClass: Array,
+def buffer_from_dict_example(example: Dict, leading_dims: Tuple[int, ...], ArrayClass: Array,
                              *, name: str, force_float32: bool = False, **kwargs) -> Buffer:
     """Create a samples buffer from an example which may be a dictionary (or
     just a single value). The samples buffer will be a NamedArrayTuple with a
