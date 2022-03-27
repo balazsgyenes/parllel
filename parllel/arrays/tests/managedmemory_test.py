8e393a158e4f0c874dbbffad09829ac8e43db9f7
Balazs Gyenes <balazs.gyenes@kit.edu>
2022-03-27 20:03:53 +0200

    Moved managed/shared memory tests into classes.

--- parllel/arrays/tests/managedmemory_test.py
+++ parllel/arrays/tests/managedmemory_test.py
@@ -45,68 +45,69 @@ def setitem_in_piped_array(pipe):
     array, location, value = pipe.recv()
     array[location] = value
 
-
-def test_setitem_single(array, np_array, mp_ctx):
-    location = (0, 1, 2)
-    value = -7
-
-    parent_pipe, child_pipe = mp_ctx.Pipe()
-    p = mp_ctx.Process(target=setitem_in_piped_array, args=(child_pipe,))
-    p.start()
-    parent_pipe.send((array, location, value))
-    p.join()
-
-    np_array[location] = value
-    assert np.array_equal(array, np_array)
-
-def test_setitem_slice(array, np_array, mp_ctx):
-    location = (3, slice(1,2))
-    value = -7
-
-    parent_pipe, child_pipe = mp_ctx.Pipe()
-    p = mp_ctx.Process(target=setitem_in_piped_array, args=(child_pipe,))
-    p.start()
-    parent_pipe.send((array, location, value))
-    p.join()
-
-    np_array[location] = value
-    assert np.array_equal(array, np_array)
-
-def test_setitem_subarray(array, np_array, mp_ctx):
-    subarray = array[2, :2]
-    subarray = subarray[1:, :]
-
-    location = (0, 3)
-    value = -7
-
-    parent_pipe, child_pipe = mp_ctx.Pipe()
-    p = mp_ctx.Process(target=setitem_in_piped_array, args=(child_pipe,))
-    p.start()
-    parent_pipe.send((subarray, location, value))
-    p.join()
-
-    np_subarray = np_array[2, :2]
-    np_subarray = np_subarray[1:, :]
-    np_subarray[location] = value
-    assert np.array_equal(array, np_array)
-
 def get_piped_array_shape(pipe):
     array, location = pipe.recv()
     subarray = array[location]
     pipe.send(subarray.shape)
 
-def test_subarray_shape(array, np_array, mp_ctx):
-    subarray = array[:3, 2]
-    subarray = subarray[:, 2:]
-    location = 1
 
-    parent_pipe, child_pipe = mp_ctx.Pipe()
-    p = mp_ctx.Process(target=get_piped_array_shape, args=(child_pipe,))
-    p.start()
-    parent_pipe.send((subarray, location))
-    subarray_shape = parent_pipe.recv()
-    p.join()
+class TestManagedMemoryArray:
+    def test_setitem_single(self, array, np_array, mp_ctx):
+        location = (0, 1, 2)
+        value = -7
 
-    np_subarray = np_array[:3, 2]
-    np_subarray = np_subarray[:, 2:]
-    assert subarray_shape == np_subarray[location].shape
+        parent_pipe, child_pipe = mp_ctx.Pipe()
+        p = mp_ctx.Process(target=setitem_in_piped_array, args=(child_pipe,))
+        p.start()
+        parent_pipe.send((array, location, value))
+        p.join()
+
+        np_array[location] = value
+        assert np.array_equal(array, np_array)
+
+    def test_setitem_slice(self, array, np_array, mp_ctx):
+        location = (3, slice(1,2))
+        value = -7
+
+        parent_pipe, child_pipe = mp_ctx.Pipe()
+        p = mp_ctx.Process(target=setitem_in_piped_array, args=(child_pipe,))
+        p.start()
+        parent_pipe.send((array, location, value))
+        p.join()
+
+        np_array[location] = value
+        assert np.array_equal(array, np_array)
+
+    def test_setitem_subarray(self, array, np_array, mp_ctx):
+        subarray = array[2, :2]
+        subarray = subarray[1:, :]
+
+        location = (0, 3)
+        value = -7
+
+        parent_pipe, child_pipe = mp_ctx.Pipe()
+        p = mp_ctx.Process(target=setitem_in_piped_array, args=(child_pipe,))
+        p.start()
+        parent_pipe.send((subarray, location, value))
+        p.join()
+
+        np_subarray = np_array[2, :2]
+        np_subarray = np_subarray[1:, :]
+        np_subarray[location] = value
+        assert np.array_equal(array, np_array)
+
+    def test_subarray_shape(self, array, np_array, mp_ctx):
+        subarray = array[:3, 2]
+        subarray = subarray[:, 2:]
+        location = 1
+
+        parent_pipe, child_pipe = mp_ctx.Pipe()
+        p = mp_ctx.Process(target=get_piped_array_shape, args=(child_pipe,))
+        p.start()
+        parent_pipe.send((subarray, location))
+        subarray_shape = parent_pipe.recv()
+        p.join()
+
+        np_subarray = np_array[:3, 2]
+        np_subarray = np_subarray[:, 2:]
+        assert subarray_shape == np_subarray[location].shape
--- parllel/arrays/tests/sharedmemory_test.py
+++ parllel/arrays/tests/sharedmemory_test.py
@@ -44,57 +44,59 @@ def array(blank_array, np_array):
     return blank_array
 
 
-def test_setitem_single(array, np_array, mp_ctx):
-    location = (0, 1, 2)
-    value = -7
-
-    p = mp_ctx.Process(target=setitem, args=(array, location, value))
-    p.start()
-    p.join()
-
-    np_array[location] = value
-    assert np.array_equal(array, np_array)
-
-def test_setitem_slice(array, np_array, mp_ctx):
-    location = (3, slice(1,2))
-    value = -7
-
-    p = mp_ctx.Process(target=setitem, args=(array, location, value))
-    p.start()
-    p.join()
-
-    np_array[location] = value
-    assert np.array_equal(array, np_array)
-
-def test_setitem_subarray(array, np_array, mp_ctx):
-    subarray = array[2, :2]
-    subarray = subarray[1:, :]
-
-    location = (0, 3)
-    value = -7
-
-    p = mp_ctx.Process(target=setitem, args=(subarray, location, value))
-    p.start()
-    p.join()
-
-    np_subarray = np_array[2, :2]
-    np_subarray = np_subarray[1:, :]
-    np_subarray[location] = value
-    assert np.array_equal(array, np_array)
-
 def get_array_shape(pipe, array):
     pipe.send(array.shape)
 
-def test_subarray_shape(array, np_array, mp_ctx):
-    subarray = array[:3, 2]
-    subarray = subarray[:, 2:]
 
-    parent_pipe, child_pipe = mp_ctx.Pipe()
-    p = mp_ctx.Process(target=get_array_shape, args=(child_pipe, subarray))
-    p.start()
-    subarray_shape = parent_pipe.recv()
-    p.join()
+class TestSharedMemoryArray:
+    def test_setitem_single(self, array, np_array, mp_ctx):
+        location = (0, 1, 2)
+        value = -7
 
-    np_subarray = np_array[:3, 2]
-    np_subarray = np_subarray[:, 2:]
-    assert subarray_shape == np_subarray.shape
+        p = mp_ctx.Process(target=setitem, args=(array, location, value))
+        p.start()
+        p.join()
+
+        np_array[location] = value
+        assert np.array_equal(array, np_array)
+
+    def test_setitem_slice(self, array, np_array, mp_ctx):
+        location = (3, slice(1,2))
+        value = -7
+
+        p = mp_ctx.Process(target=setitem, args=(array, location, value))
+        p.start()
+        p.join()
+
+        np_array[location] = value
+        assert np.array_equal(array, np_array)
+
+    def test_setitem_subarray(self, array, np_array, mp_ctx):
+        subarray = array[2, :2]
+        subarray = subarray[1:, :]
+
+        location = (0, 3)
+        value = -7
+
+        p = mp_ctx.Process(target=setitem, args=(subarray, location, value))
+        p.start()
+        p.join()
+
+        np_subarray = np_array[2, :2]
+        np_subarray = np_subarray[1:, :]
+        np_subarray[location] = value
+        assert np.array_equal(array, np_array)
+
+    def test_subarray_shape(self, array, np_array, mp_ctx):
+        subarray = array[:3, 2]
+        subarray = subarray[:, 2:]
+
+        parent_pipe, child_pipe = mp_ctx.Pipe()
+        p = mp_ctx.Process(target=get_array_shape, args=(child_pipe, subarray))
+        p.start()
+        subarray_shape = parent_pipe.recv()
+        p.join()
+
+        np_subarray = np_array[:3, 2]
+        np_subarray = np_subarray[:, 2:]
+        assert subarray_shape == np_subarray.shape
