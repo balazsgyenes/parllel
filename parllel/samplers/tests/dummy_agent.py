ec87095fe3ca5ce05f523ac5e0f8c74479455d4d
Balazs Gyenes <balazs.gyenes@kit.edu>
2022-04-06 19:11:36 +0200

    Working test case for BasicSampler.

--- parllel/samplers/tests/dummy_agent.py
+++ parllel/samplers/tests/dummy_agent.py
@@ -2,48 +2,106 @@ from typing import Optional, Union
 
 import gym
 import numpy as np
+from numpy import random
 from nptyping import NDArray
+from parllel.arrays.utils import buffer_from_dict_example, buffer_from_example
 
-from parllel.buffers import Buffer, NamedTupleClass, buffer_method, buffer_map
-from parllel.handlers import Agent, AgentStep
+from parllel.arrays import Array, buffer_from_dict_example
+from parllel.buffers import Buffer, NamedTupleClass, buffer_method, buffer_asarray
+from parllel.handlers import Agent, AgentStep, Handler
+from parllel.samplers import AgentSamples
+from parllel.types import BatchSpec
 
 
-DummyAgentInfo = NamedTupleClass("DummyAgentInfo", ["observation", "state"])
+DummyAgentInfo = NamedTupleClass("DummyAgentInfo", ["observation"])
 
 
 class DummyAgent(Agent):
     def __init__(self, action_space: gym.Space, observation_space: gym.Space,
-            recurrent: bool = False) -> None:
+            batch_spec: BatchSpec, n_batches: int, recurrent: bool = False) -> None:
         self.action_space = action_space
         self.observation_space = observation_space
+        self.batch_B = batch_spec.B
         self.recurrent = recurrent
-    
-    def dry_run(self, n_states: int, observation: Buffer):
-        agent_info = (buffer_method(observation, "copy"),)
+
+        self._step_ctr = 0
+        self._batch_ctr = 0
+        batch_action = buffer_from_dict_example(self.action_space.sample(),
+            (n_batches * batch_spec.T, batch_spec.B), Array, name="action")
+        batch_info = buffer_from_dict_example(
+            {"observation": self.observation_space.sample()},
+            (n_batches * batch_spec.T, batch_spec.B), Array, name="agentinfo")
+        self._agent_samples = AgentSamples(batch_action, batch_info)
+        self._values = buffer_from_example(np.array(0, dtype=np.float32),
+            (n_batches, batch_spec.B), Array)
+
         if self.recurrent:
-            self._n_states = n_states
-            self._states = np.zeros(shape=(n_states,))
-            state = self._states[0].copy()
-            agent_info += (state,)
-        agent_info = DummyAgentInfo(*agent_info)
-        return agent_info, state
-        
+            raise NotImplementedError
+
+        self.rng = random.default_rng()
+
     def reset(self) -> None:
-        self._states[:] = np.arange(self._n_states)
+        if self.recurrent:
+            self._states[:] = np.arange(self._n_states)
     
     def reset_one(self, env_index: int) -> None:
-        self._states[env_index] = env_index
+        if self.recurrent:
+            self._states[env_index] = env_index
 
     def step(self, observation: Buffer, *, env_indices: Union[int, slice] = ...,
              ) -> AgentStep:
-        agent_info = (buffer_method(observation, "copy"),)
-        if self.recurrent:
-            agent_info += (self._states[env_indices].copy(),)
-            self._states[env_indices] += np.arange(self._n_states)[env_indices]
-        return AgentStep(
-            action = self._states[env_indices].copy() * 10,
-            agent_info = DummyAgentInfo(*agent_info),
-        )
+        action = self.action_space.sample()
+        agent_info = DummyAgentInfo(buffer_method(observation, "copy"))
+        self._agent_samples.action[self._step_ctr] = action
+        self._agent_samples.agent_info[self._step_ctr] = agent_info
+        self._step_ctr += 1
+
+        # if self.recurrent:
+        #     agent_info += (self._states[env_indices].copy(),)
+        #     self._states[env_indices] += np.arange(self._n_states)[env_indices]
+
+        return AgentStep(action, agent_info)
 
     def value(self, observation: Buffer) -> Buffer:
-        return self._states.copy() * 10
+        value = self.rng.random(self.batch_B)
+        self._values[self._batch_ctr] = value
+        self._batch_ctr += 1
+        return value
+
+    @property
+    def agent_samples(self):
+        return self._agent_samples
+
+    @property
+    def values(self):
+        return self._values
+
+
+class DummyHandler(Handler):
+    def step(self, observation: Buffer[Array], *, env_indices:
+            Union[int, slice] = ..., out_action: Buffer[Array] = None,
+            out_agent_info: Buffer[Array] = None) -> Optional[AgentStep]:
+        
+        observation: Buffer[NDArray] = buffer_asarray(observation)
+
+        agent_step: AgentStep = self._agent.step(observation,
+            env_indices=env_indices)
+
+        if any(out is None for out in (out_action, out_agent_info)):
+            return agent_step
+        else:
+            action, agent_info = agent_step
+            out_action[:] = action
+            out_agent_info[:] = agent_info
+
+    def value(self, observation: Buffer[Array], *,
+            out_value: Buffer[Array] = None) -> Optional[Buffer]:
+        
+        observation: Buffer[NDArray] = buffer_asarray(observation)
+
+        value: Buffer[NDArray] = self._agent.value(observation)
+
+        if out_value is None:
+            return value
+        else:
+            out_value[:] = value
--- parllel/samplers/tests/test_basic.py
+++ parllel/samplers/tests/test_basic.py
@@ -1,69 +1,150 @@
 import pytest
 
+from gym import spaces
+import numpy as np
+
 from parllel.arrays import Array, RotatingArray, buffer_from_dict_example
+from parllel.buffers import buffer_asarray, buffer_method
 from parllel.cages import Cage
-from parllel.samplers import Samples, EnvSamples
+from parllel.samplers import Samples, AgentSamples, EnvSamples
 from parllel.types import BatchSpec, TrajInfo
 
 from parllel.samplers.basic import BasicSampler
 from parllel.samplers.tests.dummy_env import DummyEnv
+from parllel.samplers.tests.dummy_agent import DummyAgent, DummyHandler
 
 
-@pytest.fixture(params=[
-    (64, 8),
-    (64, 1),
-    (2, 8),
-    (1, 8),
-], scope="module")
+@pytest.fixture(
+    params=[(64, 8), (64, 1), (2, 8), (1, 8)],
+    ids=["batch=64x8", "batch=64x1", "batch=2x8", "batch=1x8"],
+    scope="module",
+)
 def batch_spec(request):
     T, B = request.param
     return BatchSpec(T, B)
 
-@pytest.fixture(scope="class")
-def envs(batch_spec):
-    return [Cage(
+@pytest.fixture(
+    params=[spaces.Box(-np.inf, np.inf, (4,), np.float32)],
+    ids=["obs=Box"],
+    scope="module",
+)
+def observation_space(request):
+    return request.param
+
+@pytest.fixture(
+    params=[spaces.Discrete(2)],
+    ids=["action=Discrete"],
+    scope="module",
+)
+def action_space(request):
+    return request.param
+
+@pytest.fixture(
+    params=[0, 25],
+    ids=["decorrelation=0", "decorrelation=25"],
+    scope="module",
+)
+def max_decorrelation_steps(request):
+    return request.param
+
+@pytest.fixture(
+    params=[False],
+    ids=["singleagent"],
+)
+def envs(request, action_space, observation_space, batch_spec):
+    episode_lengths = list(range(5, 5 + batch_spec.B))
+    cages = [Cage(
         EnvClass=DummyEnv,
-        env_kwargs={},
+        env_kwargs=dict(
+            action_space=action_space,
+            observation_space=observation_space,
+            episode_length=length,
+            batch_spec=batch_spec,
+            n_batches=2,
+            multi_reward=request.param,
+        ),
         TrajInfoClass=TrajInfo,
         traj_info_kwargs={},
         wait_before_reset=False,
-    ) for _ in range(batch_spec.B)]
+    ) for length in episode_lengths]
 
-@pytest.fixture(scope="class")
-def batch_buffer(batch_spec, envs):
-    ArrayCls = Array
-    RotatingArrayCls = RotatingArray
+    yield cages
 
-    # create example env
-    example_cage = envs[0]
+    for cage in cages:
+        cage.close()
 
+@pytest.fixture()
+def agent(action_space, observation_space, batch_spec):
+    agent = DummyAgent(
+        action_space=action_space,
+        observation_space=observation_space,
+        batch_spec=batch_spec,
+        n_batches=2,
+        recurrent=False,
+    )
+    handler = DummyHandler(agent)
+    return handler
+
+@pytest.fixture()
+def batch_buffer(action_space, observation_space, batch_spec, envs):
     # get example output from env
-    example_env_output = example_cage.get_example_output()
-    obs, reward, done, info = example_env_output
-    action = example_cage.spaces.action.sample()
+    obs, reward, done, info = envs[0].get_example_output()
 
     # allocate batch buffer based on examples
-    batch_observation = buffer_from_dict_example(obs, tuple(batch_spec), RotatingArrayCls, name="obs", padding=1)
-    batch_reward = buffer_from_dict_example(reward, tuple(batch_spec), ArrayCls, name="reward")
-    batch_done = buffer_from_dict_example(done, tuple(batch_spec), RotatingArrayCls, name="done", padding=1)
-    batch_info = buffer_from_dict_example(info, tuple(batch_spec), ArrayCls, name="envinfo")
-    batch_buffer_env = EnvSamples(batch_observation, batch_reward, batch_done, batch_info)
-
-    """In discrete problems, integer actions are used as array indices during
-    optimization. Pytorch requires indices to be 64-bit integers, so we do not
-    convert here.
-    """
-    batch_action = buffer_from_dict_example(action, tuple(batch_spec), ArrayCls, name="action", force_32bit=False)
+    batch_observation = buffer_from_dict_example(obs, tuple(batch_spec),
+        RotatingArray, name="obs", padding=1)
+    batch_reward = buffer_from_dict_example(reward, tuple(batch_spec),
+        Array, name="reward")
+    batch_done = buffer_from_dict_example(done, tuple(batch_spec),
+        Array, name="done")
+    batch_info = buffer_from_dict_example(info, tuple(batch_spec),
+        Array, name="envinfo")
+    batch_env = EnvSamples(batch_observation, batch_reward,
+        batch_done, batch_info)
+    batch_action = buffer_from_dict_example(action_space.sample(),
+        tuple(batch_spec), Array, name="action")
+    batch_agent_info = buffer_from_dict_example(
+        {"observation": observation_space.sample()},
+        tuple(batch_spec), Array, name="agentinfo")
+    batch_agent = AgentSamples(batch_action, batch_agent_info)
+    batch_buffer = Samples(batch_agent, batch_env)
 
     for env in envs:
         env.set_samples_buffer(batch_action, batch_observation, batch_reward,
             batch_done, batch_info)
 
-    yield (batch_action, batch_buffer_env)
+    yield batch_buffer
 
-@pytest.fixture
-def sampler(batch_spec, envs):
-    BasicSampler(
+    buffer_method(batch_buffer, "close")
+    buffer_method(batch_buffer, "destroy")
+
+@pytest.fixture(params=[BasicSampler]) # scope="class", 
+def samples(request, batch_spec, envs, agent, batch_buffer, max_decorrelation_steps):
+    SamplerClass = request.param
+
+    sampler = SamplerClass(
         batch_spec=batch_spec,
-        envs = envs
+        envs = envs,
+        agent = agent,
+        batch_buffer = batch_buffer,
+        max_steps_decorrelate=max_decorrelation_steps,
     )
+
+    samples1, completed_trajs1 = sampler.collect_batch(0)
+    samples1 = buffer_method(samples1, "copy")
+    samples2, completed_trajs2 = sampler.collect_batch(np.prod(tuple(batch_spec)))
+    samples2 = buffer_method(samples2, "copy")
+    
+    yield (samples1, samples2, completed_trajs1, completed_trajs2)
+
+    sampler.close()
+
+
+class TestBasicSampler:
+    def test_match(self, samples, envs, batch_spec):
+        samples1, samples2, _, _ = samples
+
+        for b, env in enumerate(envs):
+            env_samples = samples1.env[:, b]
+            reference = buffer_asarray(env._env.env_samples[:batch_spec.T])
+            assert np.array_equal(reference.observation, env_samples.observation)
