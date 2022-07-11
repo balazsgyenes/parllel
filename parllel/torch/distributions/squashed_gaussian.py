a8601d0a253de73b3db42fbf65da6b1a0312d7a9
Balazs Gyenes <balazs.gyenes@kit.edu>
2022-07-11 14:49:56 +0200

    Update docs and polish code for SquashedGaussian.

--- parllel/torch/distributions/squashed_gaussian.py
+++ parllel/torch/distributions/squashed_gaussian.py
@@ -25,52 +25,40 @@ class SquashedGaussian(Gaussian):
         raise NotImplementedError
 
     def log_likelihood(self, x, dist_info):
+        # TODO: an implementation for this exists in SB3, even though it's not
+        # very efficient
+        raise NotImplementedError
+
+    def likelihood_ratio(self, x, old_dist_info, new_dist_info):
         raise NotImplementedError
 
     def sample_loglikelihood(self, dist_info):
+        """Special method for use with SAC algorithm, which efficiently
+        computes a new sampled action and its log-likelihood for optimization
+        use. The log-likelihood requires the unsquashed sample, so instead of
+        squashing and unsquashing, the parent methods are called and the sample
+        is squashed afterwards. Then, the log-likelihood is corrected to take
+        the squashing into account. The interested reader should refer to
+        Appendix C of the SAC paper by Haarnoja et al. (2018).
+        
+        TODO: verify correction factor with scale not equal 1 (not included in
+            paper).
         """
-        Special method for use with SAC algorithm, which returns a new sampled 
-        action and its log-likelihood for training use.  Temporarily turns OFF
-        squashing, so that log_likelihood can be computed on non-squashed sample,
-        and then restores squashing and applies it to the sample before output.
-        """
+        # sample unsquashed Gaussian distribution
         sample = super().sample(dist_info)
+
+        # compute log likelihood of unsquashed sample
         logli = super().log_likelihood(sample, dist_info)
-        # TODO: where does this factor come from?
+
+        # squash sample and add correction to log likelihood
+        tanh_x = torch.tanh(sample)
         logli -= torch.sum(
-            torch.log(self.scale * (1 - torch.tanh(x) ** 2) + EPS),
+            torch.log(self.scale * (1 - tanh_x ** 2) + EPS),
             dim=-1,
         )
-        sample = self.scale * torch.tanh(sample)
+        sample = self.scale * tanh_x
+        
         return sample, logli
-
-    # def sample_loglikelihood(self, dist_info):
-    #     """Use in SAC with squash correction, since log_likelihood() expects raw_action."""
-    #     mean = dist_info.mean
-    #     log_std = dist_info.log_std
-    #     if self.min_log_std is not None or self.max_log_std is not None:
-    #         log_std = torch.clamp(log_std, min=self.min_log_std,
-    #             max=self.max_log_std)
-    #     std = torch.exp(log_std)
-    #     normal = torch.distributions.Normal(mean, std)
-    #     sample = normal.rsample()
-    #     logli = normal.log_prob(sample)
-    #     if self.scale is not None:
-    #         sample = self.scale * torch.tanh(sample)
-    #         logli -= torch.sum(
-    #             torch.log(self.scale * (1 - torch.tanh(sample) ** 2) + EPS),
-    #             dim=-1)
-    #     return sample, logli
-
-
-        # scale = self.scale
-        # self.scale = None  # Temporarily turn OFF.
-        # sample = self.sample(dist_info)
-        # self.scale = scale  # Turn it back ON, raw_sample into squash correction.
-        # logli = self.log_likelihood(sample, dist_info)
-        # if scale is not None:
-        #     sample = scale * torch.tanh(sample)
-        # return sample, logli
 
     def sample(self, dist_info):
         """
