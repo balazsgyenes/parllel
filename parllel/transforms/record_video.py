c7d57e38558357bfc014b3a9e327ccc9b9e12f37
Balazs Gyenes <balazs.gyenes@kit.edu>
2022-12-23 13:44:52 +0100

    Draft of RecordVectorizedVideo transform.
    Copy tile_images method from SB3.
    Implement a stripped down combination of VecVideoRecorder and
    gym's VideoRecorder.

---
+++ parllel/transforms/record_video.py
@@ -0,0 +1,93 @@
+from pathlib import Path
+from typing import Optional, Sequence, Tuple
+
+from gym.wrappers.monitoring import video_recorder
+import numpy as np
+
+from parllel.buffers import Samples, NamedTuple
+
+from .transform import StepTransform
+
+
+def tile_images(img_nhwc: Sequence[np.ndarray]) -> np.ndarray:  # pragma: no cover
+    """
+    Tile N images into one big PxQ image
+    (P,Q) are chosen to be as close as possible, and if N
+    is square, then P=Q.
+
+    :param img_nhwc: list or array of images, ndim=4 once turned into array. img nhwc
+        n = batch index, h = height, w = width, c = channel
+    :return: img_HWc, ndim=3
+
+    # TODO: calculate new_height and new_width once and preallocate 
+    """
+    img_nhwc = np.asarray(img_nhwc)
+    n_images, height, width, n_channels = img_nhwc.shape # TODO: verify
+    tiled_height = int(np.ceil(np.sqrt(n_images)))
+    tiled_width = int(np.ceil(float(n_images) / tiled_height))
+    img_nhwc = np.array(list(img_nhwc) + [img_nhwc[0] * 0 for _ in range(n_images, tiled_height * tiled_width)])
+    # img_HWhwc
+    out_image = img_nhwc.reshape((tiled_height, tiled_width, height, width, n_channels))
+    # img_HhWwc
+    out_image = out_image.transpose(0, 2, 1, 3, 4)
+    # img_Hh_Ww_c
+    out_image = out_image.reshape((tiled_height * height, tiled_width * width, n_channels))
+    return out_image
+
+
+class RecordVectorizedVideo(StepTransform):
+    def __init__(self,
+        record_every_n_steps: int,
+        video_length: int,
+        buffer_key_to_record: str, # e.g. "observation" or "rendering"
+        output_fps: int,
+        tiled_shape: Tuple[int], # TODO: calculate this on init
+        env_fps: int, # TODO: maybe grab this from example env metadata
+    ) -> None:
+        self.record_every = record_every_n_steps
+        self.length = video_length
+        self.key = buffer_key_to_record
+        self.output_fps = output_fps
+
+        self.total_t = 0
+        self.recording = False
+        self.tiled_shape = tiled_shape
+        self.env_fps = env_fps
+
+    def __call__(self, batch_samples: Samples, t: int) -> Samples:
+        self.total_t += 1
+
+        # check if we should start recording
+        # TODO: does this need to be corrected for recurrent batches that
+        # break early? maybe a batch transform would be better
+        if not self.recording:
+            self.recording = (self.total_t % self.record_every == 0)
+            if self.recording:
+                self._start_recording()
+
+        if self.recording:
+            images = getattr(batch_samples.env, self.key)[t]
+            images_tiled = tile_images(np.asarray(images))
+            self.recorder.capture_frame(images_tiled)
+            self.recorded_frames += 1
+            if self.recorded_frames >= self.length:
+                self._stop_recording()
+
+    def _start_recording(self) -> None:
+        self.recorder = video_recorder.ImageEncoder(
+            output_path=Path().home(), # TODO: fix
+            frame_shape=self.tiled_shape,
+            frames_per_sec=self.env_fps,
+            output_frames_per_sec=self.output_fps,
+        )
+        self.recorded_frames = 0
+
+    def _stop_recording(self) -> None:
+        # TODO: use weakref to ensure this gets closed even if training ends
+        # during video recording (transform has no close method)
+        self.recorder.close()
+        self.recording = False
+
+    def close(self) -> None:
+        # TODO: call this in the cleanup of build method
+        self._stop_recording()
