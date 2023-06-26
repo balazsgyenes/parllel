27b4a0ac313a319bfd6e72cfa31a31e39b759625
Balazs Gyenes <balazs.gyenes@kit.edu>
2023-06-26 23:13:31 +0200

    Fix indices benchmark.

--- parllel/arrays/benchmarks/indices.py
+++ parllel/arrays/benchmarks/indices.py
@@ -1,66 +1,64 @@
 import time
 
-from numba import njit
 import numpy as np
 from numpy.random import default_rng
 
-from parllel.arrays.indices import add_indices, compute_indices
+from parllel.arrays.indices import add_locations, compute_indices, init_location
+from parllel.arrays.tests.indices_test import random_location
 
 
-n_repeat = 100
-base_shape = (10, 10, 10, 10)
+N_REPEAT = 1000
+BASE_SHAPE = (20, 20, 20, 20)
 
-single = (4, 7, 3)
-
-multiple = [
-    (2, slice(None)),
-    (slice(1, None), slice(2, -1), 5),
-    (6, slice(None, 4)),
-]
-
-
-def random_indices(rng):
-    pass
 
 def benchmark_multiple_indices(rng):
 
-    times_add_indices = np.zeros(n_repeat, dtype=float)
-    times_index = np.zeros(n_repeat, dtype=float)
-    times_compute_indices = np.zeros(n_repeat, dtype=float)
+    times_add_indices = np.zeros(N_REPEAT, dtype=float)
+    times_index = np.zeros(N_REPEAT, dtype=float)
+    times_compute_indices = np.zeros(N_REPEAT, dtype=float)
 
-    for n in range(n_repeat):
+    for n in range(N_REPEAT):
 
-        np_array = rng.random(base_shape, dtype=np.float32)
-        current_indices = []
-        for location in multiple:
-            start = time.perf_counter_ns()
+        np_array = rng.random(BASE_SHAPE, dtype=np.float32)
 
-            current_indices = add_indices(base_shape, current_indices, location)
+        current_location = init_location(BASE_SHAPE)
 
-            end = time.perf_counter_ns()
-            times_add_indices[n] += (end - start) / 1000
+        loc1 = random_location(rng, np_array.shape, max_step=3, prob_start_stop_negative=0.5, prob_step_negative=0.5)
+        subarray = np_array[loc1]
+        current_location = add_locations(current_location, loc1, np_array.shape)
 
+        if not subarray.shape:
+            continue  # if we have indexed a single element already, skip
 
-            start = time.perf_counter_ns()
-
-            next_array = np_array[location]
-
-            end = time.perf_counter_ns()
-            times_index[n] += (end - start) / 1000
+        loc2 = random_location(rng, subarray.shape, max_step=3, prob_start_stop_negative=0.5, prob_step_negative=0.5)
 
 
-            start = time.perf_counter_ns()
+        start = time.perf_counter_ns()
 
-            computed_indices = compute_indices(np_array, np_array)
+        current_location = add_locations(current_location, loc2, np_array.shape)
 
-            end = time.perf_counter_ns()
-            times_compute_indices[n] += (end - start) / 1000
+        end = time.perf_counter_ns()
+        times_add_indices[n] += (end - start) / 1000
 
-            # assert computed_indices == current_indices
-            np_array = next_array
 
-    print(f"Adding indices = {times_add_indices.mean():.3f} +/- {times_add_indices.std():.3f} us")
+        start = time.perf_counter_ns()
+
+        subarray = subarray[loc2]
+
+        end = time.perf_counter_ns()
+        times_index[n] += (end - start) / 1000
+
+
+        start = time.perf_counter_ns()
+
+        computed_located = compute_indices(np_array, subarray)
+
+        end = time.perf_counter_ns()
+        times_compute_indices[n] += (end - start) / 1000
+
+
     print(f"Indexing = {times_index.mean():.3f} +/- {times_index.std():.3f} us")
+    print(f"Adding indices = {times_add_indices.mean():.3f} +/- {times_add_indices.std():.3f} us")
     print(f"Computing indices = {times_compute_indices.mean():.3f} +/- {times_compute_indices.std():.3f} us")
 
 
