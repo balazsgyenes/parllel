1cea420d440dd4bf1a2d1651a2c2c2f43ccfaca9
Nikolai Franke <nikolai.franke@student.kit.edu>
2023-06-26 13:47:03 +0200

    Formatted samples

--- parllel/buffers/samples.py
+++ parllel/buffers/samples.py
@@ -1,12 +1,11 @@
 from .named_tuple import NamedArrayTupleClass
 
-
 Samples = NamedArrayTupleClass("Samples", ["agent", "env"])
 
 
-AgentSamples = NamedArrayTupleClass("AgentSamples",
-    ["action", "agent_info"])
+AgentSamples = NamedArrayTupleClass("AgentSamples", ["action", "agent_info"])
 
 
-EnvSamples = NamedArrayTupleClass("EnvSamples",
-    ["observation", "reward", "done", "env_info"])
+EnvSamples = NamedArrayTupleClass(
+    "EnvSamples", ["observation", "reward", "done", "env_info"]
+)
