d424c76cbf12d511a2fe9c2e22aceb0a9730b529
Balazs Gyenes <balazs.gyenes@kit.edu>
2023-07-18 01:39:25 +0200

    Delete Array utils.

--- parllel/arrays/__init__.py
+++ parllel/arrays/__init__.py
@@ -2,7 +2,6 @@
 import parllel.buffers
 
 from .array import Array
-from .utils import buffer_from_example, buffer_from_dict_example
 
 # these types are only imported so they can be registered
 from .jagged import JaggedArray
@@ -12,5 +11,4 @@ from .sharedmemory import SharedMemoryArray
 
 __all__ = [
     "Array",
-    "buffer_from_example", "buffer_from_dict_example",
 ]
