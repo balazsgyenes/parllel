b6b3a98f10fceb5c304bd3541fc4bd6e4172e558
Nikolai Franke <nikolai.franke@gmail.com>
2023-08-15 16:18:46 +0200

    Small fix

--- parllel/jax/models.py
+++ parllel/jax/models.py
@@ -9,7 +9,6 @@ class DistParams(TypedDict):
     probs: ArrayLike
     # log_std: ArrayLike
 
-
 class ModelOutputs(TypedDict):
     dist_params: DistParams
     value: NotRequired[ArrayLike]
@@ -18,10 +17,10 @@ class ActorCriticModel(nn.Module):
     actor_hidden_sizes: Sequence[int]
     critic_hidden_sizes: Sequence[int]
     action_dim: int
-
     @nn.compact
     def __call__(self, input):
-        x = input.astype(jnp.float32)
+        input = input.astype(jnp.float32)
+        x = input
         for size in self.actor_hidden_sizes:
             x = nn.Dense(features=size)(x)
             x = nn.relu(x)
