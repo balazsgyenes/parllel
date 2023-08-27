76c86666c25221c709f6aa05b07b52a91176179e
Balazs Gyenes <balazs.gyenes@kit.edu>
2023-08-27 14:59:30 +0200

    Fix train_all script.

--- benchmarks/sb3/train_all.sh
+++ benchmarks/sb3/train_all.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-python benchmarks/sb3/train_sac_cheetah_parllel.py -m +iteration="range(5)"
-python benchmarks/sb3/train_sac_hopper_parllel.py -m +iteration="range(5)"
-python benchmarks/sb3/train_sac_cheetah_sb3.py -m +iteration="range(5)"
-python benchmarks/sb3/train_sac_hopper_sb3.py -m +iteration="range(5)"
+# get path to this script
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+python $SCRIPT_DIR/train_sac_cheetah_parllel.py -m +iteration="range(5)"
+python $SCRIPT_DIR/train_sac_hopper_parllel.py -m +iteration="range(5)"
+python $SCRIPT_DIR/train_sac_cheetah_sb3.py -m +iteration="range(5)"
+python $SCRIPT_DIR/train_sac_hopper_sb3.py -m +iteration="range(5)"
