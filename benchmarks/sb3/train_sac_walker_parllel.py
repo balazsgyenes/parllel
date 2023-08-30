47b5db2b3ec8dd6290e9b437a5277da68e6751b3
Balazs Gyenes <balazs.gyenes@kit.edu>
2023-08-30 15:13:47 +0200

    Add benchmark on Walker2d.
    Factor out common hydra configs.

---
+++ benchmarks/sb3/train_sac_walker_parllel.py
@@ -0,0 +1,43 @@
+# fmt: off
+import multiprocessing as mp
+
+# isort: off
+import hydra
+import wandb
+from hydra.core.hydra_config import HydraConfig
+from omegaconf import DictConfig, OmegaConf
+
+# isort: on
+import parllel.logger as logger
+
+# isort: split
+from build_parllel_sac import build
+
+
+# fmt: on
+@hydra.main(version_base=None, config_path="conf", config_name="sac_walker_parllel")
+def main(config: DictConfig) -> None:
+    run = wandb.init(
+        project="parllel",
+        tags=["sac", "walker"],
+        config=OmegaConf.to_container(config, resolve=True, throw_on_missing=True),
+        sync_tensorboard=True,  # auto-upload any values logged to tensorboard
+        save_code=True,  # save script used to start training, git commit, and patch
+    )
+
+    logger.init(
+        wandb_run=run,
+        log_dir=HydraConfig.get().runtime.output_dir,
+        tensorboard=True,
+    )
+
+    with build(config) as runner:
+        runner.run()
+
+    logger.close()
+    run.finish()
+
+
+if __name__ == "__main__":
+    mp.set_start_method("forkserver")
+    main()
