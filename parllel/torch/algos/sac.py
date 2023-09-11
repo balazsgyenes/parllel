8f35b4cb87aee7b440fc5f453bd5784eff28739e
Nikolai Franke <nikolai.franke@gmail.com>
2023-09-11 11:58:17 +0200

    Updates SAC

--- parllel/torch/algos/sac.py
+++ parllel/torch/algos/sac.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from collections import defaultdict
+from collections.abc import Sequence
 from itertools import chain
 
 import numpy as np
@@ -36,6 +37,7 @@ class SAC(Algorithm):
         clip_grad_norm: float,
         ent_coeff: float,
         ent_coeff_lr: float | None = None,
+        learning_rate_schedulers: Sequence[LRScheduler] | None = None,
         **kwargs,  # ignore additional arguments
     ):
         """Save input arguments."""
@@ -170,6 +172,8 @@ class SAC(Algorithm):
         q1, q2 = self.agent.q(observation, samples["action"])
         q_loss = 0.5 * valid_mean((y - q1) ** 2 + (y - q2) ** 2)
         self.algo_log_info["critic_loss"].append(q_loss.item())
+        self.algo_log_info["mean_ent_bonus"].append(entropy_bonus.mean().item())
+        self.algo_log_info["ent_coeff"].append(entropy_coeff.item())
 
         # update Q model parameters according to Q loss
         self.q_optimizer.zero_grad()
@@ -201,6 +205,8 @@ class SAC(Algorithm):
         pi_loss = valid_mean(pi_losses)
         self.algo_log_info["q_min"].append(min_q.min().item())
         self.algo_log_info["q_max"].append(min_q.max().item())
+        self.algo_log_info["target_q_min"].append(min_target_q.min().item())
+        self.algo_log_info["target_q_max"].append(min_target_q.max().item())
 
         # update Pi model parameters according to pi loss
         self.pi_optimizer.zero_grad()
