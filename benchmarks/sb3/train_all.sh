0deaa5983c82edafdead56b812d180c56ef7a5fe
Balazs Gyenes <balazs.gyenes@kit.edu>
2023-09-20 10:17:56 +0200

    Add PPO on Hopper and Walker with SB3 and parllel.

--- benchmarks/sb3/train_all.sh
+++ benchmarks/sb3/train_all.sh
@@ -8,9 +8,13 @@ python $SCRIPT_DIR/train_sac_parllel.py --config-name sac_parllel_hopper -m +ite
 python $SCRIPT_DIR/train_sac_parllel.py --config-name sac_parllel_walker -m +iteration="range(5)"
 
 python $SCRIPT_DIR/train_ppo_parllel.py --config-name ppo_parllel_cheetah -m +iteration="range(5)"
+python $SCRIPT_DIR/train_ppo_parllel.py --config-name ppo_parllel_hopper -m +iteration="range(5)"
+python $SCRIPT_DIR/train_ppo_parllel.py --config-name ppo_parllel_walker -m +iteration="range(5)"
 
 python $SCRIPT_DIR/train_sac_sb3.py --config-name sac_sb3_cheetah -m +iteration="range(5)"
 python $SCRIPT_DIR/train_sac_sb3.py --config-name sac_sb3_hopper -m +iteration="range(5)"
 python $SCRIPT_DIR/train_sac_sb3.py --config-name sac_sb3_walker -m +iteration="range(5)"
 
 python $SCRIPT_DIR/train_ppo_sb3.py --config-name ppo_sb3_cheetah -m +iteration="range(5)"
+python $SCRIPT_DIR/train_ppo_sb3.py --config-name ppo_sb3_hopper -m +iteration="range(5)"
+python $SCRIPT_DIR/train_ppo_sb3.py --config-name ppo_sb3_walker -m +iteration="range(5)"
